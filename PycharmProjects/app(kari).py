04490cf0404a4a2335b6a123d9d46202eec9dfc4
Tadataka-Matsumoto <tdtk1538.gmail.com>
2020-10-24 12:17:11 +0900

    procfileとrequirement.txtを追加

---
+++ PycharmProjects/app(kari).py
@@ -0,0 +1,21 @@
+#you tubeを真似てみる
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == 'GET':
+        pass
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        post = request.form.get('post')
+        create_post(name, post)
+    
+    posts = get_posts()
+
+    return render_template('index.html', posts=posts)
+
